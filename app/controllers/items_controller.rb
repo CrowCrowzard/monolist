60495191e4ef9ae9379d9b49fd141b566267f96c
crowcrowzard <crowizard@gmail.com>
2016-03-21 03:59:38 +0000

    add image link

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :logged_in_user , except: [:show]
   before_action :set_item, only: [:show]
+  before_action :set_have_users, only: [:show]
+  before_action :set_want_users, only: [:show]
 
   def new
     if params[:q]
@@ -18,5 +20,11 @@ class ItemsController < ApplicationController
   private
   def set_item
     @item = Item.find(params[:id])
+  end
+  
+  def set_have_users
+  end
+  
+  def set_want_users
   end
 end
