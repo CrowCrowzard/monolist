0d9050a483cb953707142dbb6a5afded075851e6
crowcrowzard <crowizard@gmail.com>
2016-03-27 11:24:09 +0000

    need login if wanna show ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,9 +1,19 @@
 class RankingController < ApplicationController
+  before_action :logged_in_user
+  
   def have
     @rankings = Have.group(:item_id).order('count_id desc').limit(10).count(:id) 
   end
 
   def want
     @rankings = Want.group(:item_id).order('count_id desc').limit(10).count(:id) 
+  end
+  
+  private
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to root_path
+    end
   end
 end
